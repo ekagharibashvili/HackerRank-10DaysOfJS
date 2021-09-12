980d2581c2ba80922c48df8963ea391a7e62a600
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-09-12 18:21:28 +0400

    feat: add code for Day1

---
+++ 10_Days_of_Javascript/functions(Day1).js
@@ -0,0 +1,12 @@
+function factorial(n) {
+    let result = 1;
+    if(n==0 || n==1) {
+        return result;
+    } else {
+        for(let i = n; i >=1; i--) {
+            result = result * i;
+        }
+        return result;
+    }
+}
+console.log(factorial(5));
