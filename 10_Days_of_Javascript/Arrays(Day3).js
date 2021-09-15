dc787460a351de1e3b7921ab54f27d743436fb0d
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-09-15 21:55:59 +0400

    fix: fix bugs in code

--- 10_Days_of_Javascript/Arrays(Day3).js
+++ 10_Days_of_Javascript/Arrays(Day3).js
@@ -1,7 +1,7 @@
 function getSecondLargest(nums) {
     nums = [...new Set(nums)]; // it removes duplicates from array
-    const max = Math.max(...nums);
+    let max = Math.max(...nums);
     const index = nums.indexOf(max);
     nums.splice(index, 1);
-    return nums.splice((nums.indexOf(max)), 1).join();
+    return Math.max(...nums);
 }
