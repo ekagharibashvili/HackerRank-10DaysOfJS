b0479163d7c64380c8c85e68f21777cf47db8de4
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2021-12-29 17:59:02 +0400

    feat: add test example

---
+++ 10_Days_of_Javascript/test.js
@@ -0,0 +1,22 @@
+ const fruits = [
+    { apple: 3, },
+    { pear: 9, },
+    { apple: 13 },
+    { pear: 5 },
+];
+
+function fruitsObj(arr) {
+    return arr.reduce(function (a, b) {
+        for (const [key, value] of Object.entries(b)) {
+            if (key in a) {
+                a[key] += value
+            }
+            else {
+                a[key] = value
+            }
+        }
+        return a;
+    }, {});
+}
+
+console.log(fruitsObj(fruits)); 
