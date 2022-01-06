88059bc94526a9df42fee11829193876b83f396a
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2022-01-06 19:16:20 +0400

    feat: refactor code

--- 10_Days_of_Javascript/Create a Button(Day 8)/js/button.js
+++ 10_Days_of_Javascript/Create a Button(Day 8)/js/button.js
@@ -1,8 +1,7 @@
-
 const btn = document.getElementById("btn");
-   let count = 0;
-   btn.innerHTML = count;
-  btn.addEventListener('click', function(){
-count++
+let count = 0;
+btn.innerHTML = count;
+btn.addEventListener('click', function () {
+  count++;
   btn.innerHTML = count;
 })
