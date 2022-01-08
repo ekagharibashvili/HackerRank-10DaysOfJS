1255ed652cde570f4c6af952ea60ad32c2e2b8af
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2022-01-09 03:16:23 +0400

    feat: add task

--- 10_Days_of_Javascript/Buttons_Container(Day_8)/js/buttonsGrid.js
+++ 10_Days_of_Javascript/Buttons_Container(Day_8)/js/buttonsGrid.js
@@ -1,23 +1,20 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById("btn6");
-const btn7 = document.getElementById("btn7");
-const btn8 = document.getElementById("btn8");
-const btn9 = document.getElementById("btn9");
+let l = "4";
+let a = ["1", "2", "3", "6", "9", "8", "7", "4"];
+let b = ["1", "2", "3", "6", "9", "8", "7", "4"];
 
-btn5.addEventListener('click', function() {
-let arr = [btn1, btn2, btn3, btn6, btn9, btn8, btn7, btn4]
-  for(let i = 0; i < arr.length; i++) {
-      if(i === 0){
-          arr[i] = arr[arr.length-1]
-      }
-      else {
-   arr[i] = arr[i-1]
-      }
-   console.log(arr)
+let rotate = function() {
+    for (let i = 7; i > 0; i--) {
+        a[i] = a[i - 1];
+        console.log(a[i])
+    }
+    
+    a[0] = l;
+    console.log(a[0])
+    l = a[7];
+    console.log(l)
+    
+    
+    for (let i = 0; i < 8; i++) {
+        document.getElementById("btn" + b[i]).innerText = a[i];
+    }
 }
-return arr
-})
